fd1cee22fde8f9507d2b3ea3fac89ffeb4a83537
Shane Curtis <shanecurtis1705@gmail.com>
2024-03-25 16:52:45 +1300

    Started header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 export const App = () => {
   return (
-    <div className="flex justify-center h-screen w-full bg-slate-600">
+    <div className="flex flex-col justify-center h-screen w-full bg-slate-600">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl">Shane Curtis,</h1>
+        <h3 className="text-2xl">Full stack developer</h3>
+      </div>
       <div className="h-screen mx-20 w-full max-w-[1200px]">
         <h1>Hello world</h1>
       </div>
