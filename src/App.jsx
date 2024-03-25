58f0f73cf011c2363c3d5bd368acfb8a76313d8f
Shane Curtis <shanecurtis1705@gmail.com>
2024-03-25 16:40:15 +1300

    Created the starter

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className="flex justify-center h-screen w-full bg-slate-600">
+      <div className="h-screen mx-20 w-full max-w-[1200px]">
+        <h1>Hello world</h1>
+      </div>
+    </div>
+  );
+};
